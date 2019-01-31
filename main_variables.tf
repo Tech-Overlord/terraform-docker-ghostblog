fd357acbef24f20954a974bd451ea3e7ff117a28
amuhammad <amuhammad@ctacorp.com>
2019-01-31 13:55:00 -0500

    docker orchestration

---
+++ main_variables.tf
@@ -0,0 +1,50 @@
+# Setting variables to utilize.
+variable "env"
+{
+  description = "environment can be dev or prod"
+}
+
+
+variable "container_image"
+{
+  description = "This is the image for container"
+  type = "map"
+  default =
+  {
+    dev = "ghost:latest"
+    prod = "ghost:alpine"
+  }
+}
+
+variable "container_name"
+{
+  description = "This is the name for container"
+  type = "map"
+  default
+  {
+    dev = "dev_ghostblog"
+    prod = "prod_ghostblog"
+  }
+}
+
+variable "int_port"
+{
+  description = "This is the internal port for container"
+  type = "map"
+  default
+  {
+    dev = "2368"
+    prod = "2368"
+  }
+}
+
+variable "ext_port"
+{
+  description = "This is the external port for container"
+  type = "map"
+  default
+  {
+    dev = "8080"
+    prod = "80"
+  }
+}
